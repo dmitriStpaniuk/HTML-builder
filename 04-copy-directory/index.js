7fa060cb792dd14b38c859d755440747b8514afe
Dmitry.Stepaniuk <polevaiabrest@gmail.com>
2022-05-21 20:06:15 +0300

    feat: 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+const getFixturePath = path.join(__dirname, 'files'); //?
+const getOutputPath = path.join(__dirname, './files-copy'); //?
+
+const dir = () => {
+  fs.access(getOutputPath, (err) => {
+    if (err) {
+      console.log('Creating files-copy folder...');
+      copyDir();
+    } else {
+      fs.rm(getOutputPath, { recursive: true }, (err) => {
+        if (err) console.log(err);
+        copyDir();
+      });
+    }
+  });
+};
+
+const copyDir = () => {
+
+  fs.access(getOutputPath, (err) => {
+    if (err) {
+      fs.mkdir(getOutputPath, (err) => {
+        if (err) console.log(err);
+      });
+    }
+    fs.readdir(getFixturePath, (err, files) => {
+      if (err) console.log(err);
+
+      files.map((i) => {
+        fs.copyFile(
+          getFixturePath + '/' + i,
+          getOutputPath + '/' + i,
+          (err) => {
+            if (err) console.log(err);
+          }
+        );
+      }); //?
+    });
+  });
+};
+dir();
 
