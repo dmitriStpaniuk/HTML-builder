2308b9e1f317aa03d6434e09afc26fca73c931ef
Dmitry.Stepaniuk <polevaiabrest@gmail.com>
2022-05-24 20:33:15 +0300

    feat: little fix

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -6,5 +6,5 @@ const stream = fs.ReadStream( x,'utf-8');
 let data = '';
 
 stream.on('data', chunk=> data += chunk);
-stream.on('end', ()=> console.log('End', data));
+stream.on('end', ()=> console.log(data));
 stream.on('error', error=> console.log('Error', error.message));
